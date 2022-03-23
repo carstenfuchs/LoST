766146b2b02fa3880d566ca19a053c9cd6bc1700
Carsten Fuchs <carsten.fuchs@cafu.de>
2022-03-23 12:08:40 +0100

    Add automatic backlog support for network timeouts

--- lost/network_handler.py
+++ lost/network_handler.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import dbm.gnu
 import requests
 import time
 
@@ -9,7 +10,7 @@ from thread_tools import start_thread
 REQUEST_TIMEOUT = 8.0
 
 
-def post_stamp_event(user_input):
+def post_stamp_event(user_input, is_backlog):
     """Sends the smartcard details in a POST request to the server."""
     SERVER_NAME = settings.SERVER_ADDRESS[0]
     SERVER_PORT = settings.SERVER_ADDRESS[1]
@@ -22,6 +23,7 @@ def post_stamp_event(user_input):
         {
             'terminal_name': settings.TERMINAL_NAME,
             'terminal_pwd': settings.TERMINAL_PASSWORD,
+            'is_backlog': is_backlog,
         }
     )
 
@@ -33,19 +35,19 @@ def post_stamp_event(user_input):
             verify=False,
         )
     except requests.exceptions.Timeout as e:
-        return ({'errors': [str(e)]},)
+        return user_input, {'errors': [str(e)]}, False
 
     if r.status_code != 200:
-        return ({'errors': ["status != 200"]},)
+        return user_input, {'errors': ["status != 200"]}, False
 
     try:
         json = r.json()
     except requests.exceptions.JSONDecodeError as e:
-        return ({'errors': [str(e)]},)
+        return user_input, {'errors': [str(e)]}, True
 
-    # The result of this thread is passed as a parameter to the callback
+    # The results of this thread are passed as parameters to the callback
     # in the main thread.
-    return (json,)
+    return user_input, json, True
 
 
 class NetworkHandler:
@@ -53,6 +55,8 @@ class NetworkHandler:
     def __init__(self, terminal):
         # TODO: Can we employ connection pooling?
         self.terminal = terminal
+        self.backlog = dbm.gnu.open('backlog.db', 'cs')
+        self.time_next_backlog = 0
         self.time_last_sending = 0
         self.logfile = open(settings.SMARTCARD_LOGFILE_PATH, mode='a', buffering=1)
 
@@ -60,6 +64,7 @@ class NetworkHandler:
         # TODO: Should use a context manager instead!
         # See https://realpython.com/python-with-statement/
         self.logfile.close()
+        self.backlog.close()
 
     def send_to_Lori(self, smartcard_id):
         print(f"{datetime.now()} captured {smartcard_id}", file=self.logfile)
@@ -80,8 +85,42 @@ class NetworkHandler:
         # Add the user input that was made in the terminal.
         user_input.update(self.terminal.get_user_input())
 
-        start_thread(post_stamp_event, (user_input,), self.on_server_reply)
+        is_backlog = False
+        start_thread(post_stamp_event, (user_input, is_backlog), self.on_server_reply)
 
-    def on_server_reply(self, msg):
-        print(f"{datetime.now()} server reply: {msg}", file=self.logfile)
-        self.terminal.on_server_reply_received(msg)
+    def on_server_reply(self, user_input, result, network_success):
+        print(f"{datetime.now()} server reply: {result}", file=self.logfile)
+
+        if not network_success:
+            # Something went wrong with the network transmission. Very likely, the network
+            # connectivity was interrupted, the transmission timed out and the Lori server
+            # never received the message. Thus, try again later.
+            now = time.time()
+            unique_id = user_input['local_ts']
+            self.backlog[unique_id] = json.dumps(user_input)
+            self.backlog.sync()
+            self.time_next_backlog = max(self.time_next_backlog, now + 300.0)
+
+        # if the user_input['local_ts'] is older than 1 minute:
+        #     # Either don't show anything at all or just show something simple, such as
+        #     # "Backlog: Eingabe zur Mitarbeiterkarte 1234 von 11:11 Uhr erfolgreich nachgetragen."
+        #     return
+
+        self.terminal.on_server_reply_received(result)
+
+    def catch_up_backlog(self):
+        """If there is anything in the backlog, try to file it now."""
+        if not self.backlog:
+            return
+
+        now = time.time()
+        if now < self.time_next_backlog:
+            return
+
+        unique_id, user_input_json = self.backlog.popitem()   # LIFO, unfortunately.
+        self.backlog.sync()
+        self.time_next_backlog = max(self.time_next_backlog, now + 2*REQUEST_TIMEOUT)
+
+        user_input = json.load(user_input_json)
+        is_backlog = True
+        start_thread(post_stamp_event, (user_input, is_backlog), self.on_server_reply)
